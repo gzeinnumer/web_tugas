dbaa246ebc86956e5e9991e3ef9718a20da88065
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-10-30 15:19:20 +0700

    commit

--- index.php
+++ index.php
@@ -38,6 +38,11 @@ class Fruit
     {
         return "Fruits are very healthy";
     }
+
+    public function getDetail()
+    {
+        return $this->getName() . "_" . $this->getColor() . "_" . $this->getFlavor() . "<br>";
+    }
 }
 
 class Hamper
@@ -55,6 +60,36 @@ class Hamper
     }
 }
 
+class SweetFruit extends Fruit
+{
+    public $code;
+    public function __construct($name, $color, $netto = 150, $flavor = "sweet", $code)
+    {
+        parent::__construct($name, $color, $netto, $flavor);
+        $this->code = $code;
+    }
+
+    public function sayHello()
+    {
+        return "This is Sweet Fruit, you like it?" . parent::getDetail();
+    }
+}
+class NoSweetFruit extends Fruit
+{
+    public $sugar;
+    public function __construct($name, $color, $netto = 150, $flavor = "sweet", $sugar)
+    {
+        parent::__construct($name, $color, $netto, $flavor);
+        $this->sugar = $sugar;
+    }
+
+    public function sayHello()
+    {
+        return "This is No Sweet Fruit, you like it?" . parent::getDetail();
+    }
+}
+
+
 $apple = new Fruit("Apple", "Red", 200, "sweet");
 echo $apple->getName() . "<br>";
 echo $apple->getColor() . "<br>";
@@ -64,3 +99,12 @@ echo $apple->getFlavor() . "<br>";
 $hamper = new Hamper($apple);
 
 echo $hamper->getData();
+
+
+$a = new SweetFruit("Lecy", "Red", 50, "Sweet", 911);
+
+echo $a->sayHello();
+
+$b = new NoSweetFruit("Tabu Mantah", "Gray", 500, "No Sweet", 143);
+
+echo $b->sayHello();
